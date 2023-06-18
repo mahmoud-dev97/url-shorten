9a3d65c04790dfd1db09affdd0fbab7f90a4055b
wezaa <wezaa.mahmoud@gmail.com>
2023-06-18 20:37:08 +0300

    update app and add valid

--- src/components/Shorten.jsx
+++ src/components/Shorten.jsx
@@ -1,53 +1,62 @@
 import axios from "axios";
-import { Fragment, useEffect, useState } from "react";
+import { useState } from "react";
 
 function Shorten() {
-  const [links, setLinks] = useState("");
-  const [result, setResult] = useState(null);
+  const [result, setResult] = useState("");
   const [input, setInput] = useState("");
+  const [err, setErr] = useState(false);
+  const [disable, setDisable] = useState(true);
   const [copy, setCopy] = useState("Copy");
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const regEx =
-    /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
+  const urlRegex =
+    /^(?:https?:\/\/)?(?:www\.)?([\w-]+(?:\.[\w-]+)+)(?:\/[\w-]*)*\/?$/;
 
-  useEffect(() => {
-    if (input.length > 0 && input.match(regEx)) {
-      const getShortenLink = () => {
-        axios
-          .get(`https://api.shrtco.de/v2/shorten?url=${input}`)
-          .then((res) => setResult(res.data.result));
-      };
-      getShortenLink();
+  const getShortenLink = () => {
+    axios.get(`https://api.shrtco.de/v2/shorten?url=${input}`).then((res) => {
+      console.log(res);
+      setResult(res.data.result);
+    });
+  };
+
+  const handelChange = (e) => {
+    setInput(e.target.value);
+    if (input.match(urlRegex)) {
+      setErr(false);
+      setDisable(false);
     } else {
-      setLinks("");
+      setErr(true);
+      setDisable(true);
+      setResult("");
     }
-  }, [input, regEx]);
+  };
+
   return (
-    <Fragment>
+    <>
       <div className="row p-5 shorten rounded">
         <div className="col-lg-10 col-12">
           <input
             className="form-control ps-4"
-            type="search"
+            type="url"
             placeholder="Shorten a link here..."
             aria-label="Search"
             value={input}
-            onChange={(e) => setInput(e.target.value)}
+            onChange={handelChange}
           />
         </div>
         <div className="col-lg-2 col-12">
           <button
             onClick={() => {
-              setLinks(result);
               setCopy("Copy");
+              getShortenLink();
             }}
             className="btn btn-primary w-100 mt-4 mt-lg-0"
+            disabled={disable}
           >
             Shorten It
           </button>
         </div>
       </div>
-      {links ? (
+      {err && <p className="text-danger">Please enter a valid url</p>}
+      {result !== "" && (
         <div className="row mt-3 rounded justify-lg-content-between shadow p-3 align-items-center">
           <div className="col-lg-6 col-12 ">
             <h4 className="mb-0 text-start">{input}</h4>
@@ -74,10 +83,8 @@ function Shorten() {
             </div>
           </div>
         </div>
-      ) : (
-        ""
       )}
-    </Fragment>
+    </>
   );
 }
 
